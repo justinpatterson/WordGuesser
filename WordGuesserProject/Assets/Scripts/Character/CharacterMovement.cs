d7818fb21c69c563a60823124c6548bad7192e30
Justin Patterson <justinpatterson@live.com>
2019-01-20 18:49:21 -0500

    network examples

---
+++ WordGuesserProject/Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CharacterMovement : NetworkBehaviour {
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * Time.deltaTime * 50f);
+	}
+}
