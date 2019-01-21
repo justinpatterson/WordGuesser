5b977652a392ac8604cdbff0e6d3a6aa3b97b2ad
Justin Patterson <justinpatterson@live.com>
2019-01-20 20:15:54 -0500

    basic networking name

--- WordGuesserProject/Assets/Scripts/Character/CharacterNetworkBehavior.cs
+++ WordGuesserProject/Assets/Scripts/Character/CharacterNetworkBehavior.cs
@@ -5,11 +5,33 @@ using UnityEngine.Networking;
 
 public class CharacterNetworkBehavior : NetworkBehaviour
 {
+    [SyncVar]
+    public string name = "player";
+
     private void Start()
     {
         if (isLocalPlayer)
         {
             GetComponent<CharacterMovement>().enabled = true;
         }
+    }
+
+    void OnGUI()
+    {
+        if (isLocalPlayer)
+        {
+            name = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), name);
+            if (GUI.Button(new Rect(130, Screen.height - 40, 100, 30), "UPDATE"))
+            {
+                CmdNameChanged(name);
+            }
+        }
+    }
+
+    [Command]
+    public void CmdNameChanged(string inputName)
+    {
+        name = inputName;
+        this.GetComponentInChildren<TextMesh>().text = name;
     }
 }
